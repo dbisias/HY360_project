95d09e6b824ee19a8b257474e762d768fd4fa8cb
orchiot <orchiot@ics.forth.gr>
2022-01-25 10:02:16 +0200

    Changes to be committed: 	modified:   src/main/java/Database/Tables/CompanyTable.java 	modified:   src/main/java/Database/Tables/GenericTableInterface.java 	modified:   src/main/java/Database/Tables/IndividualTable.java

--- src/main/java/Database/Tables/GenericTableInterface.java
+++ src/main/java/Database/Tables/GenericTableInterface.java
@@ -11,6 +11,7 @@ interface DBTable {
 
 //    public void addNewAccount(Account account) throws SQLException, ClassNotFoundException;
     public void createTable() throws SQLException, ClassNotFoundException;
+    public void delAccount(int acc_id) throws SQLException, ClassNotFoundException;
     public int buy(int cli_id, int mer_id, double amount) throws ClassNotFoundException, SQLException;
 //    public String accountToJSON(Account account);
     public Account findAccount(String username, String password) throws SQLException, ClassNotFoundException;
